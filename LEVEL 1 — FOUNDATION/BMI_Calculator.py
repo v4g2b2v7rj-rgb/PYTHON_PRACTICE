82c265ea410283fadc6a5176ce337612b9af09e5
v4g2b2v7rj-rgb <kushagrajais0224@gmail.com>
2026-03-09 13:55:07 +0530

    Update BMI_Calculator.py

--- LEVEL 1 — FOUNDATION/BMI_Calculator.py
+++ LEVEL 1 — FOUNDATION/BMI_Calculator.py
@@ -1,6 +1,18 @@
-weight = float(input("Enter weight (kg): "))
-height = float(input("Enter height (m): "))
-
-bmi = weight / (height ** 2)
-
-print("BMI =", bmi)
+while True:
+        print("---------------" \
+    "bmi calculator" \
+    "---------------")
+        try:
+          weight=float(input("please enter your weight: "))
+          height=float(input("please enter your height: "))
+        except Exception:
+           print("something went wrong")
+        else:
+         bmi = weight/height**2
+        finally:
+            print("your bmi is" , bmi)
+        c = int(input("press 1 to exit,2 to continue: "))
+        if c==1:
+            break
+        elif c==2:
+            continue
